68431c95de583b41d34ecfa092dd9832c5613506
RomPiot <romainpiot.pro@gmail.com>
2023-08-26 17:56:50 +0200

    feat: update dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -20,6 +20,21 @@ class DashboardController extends AbstractController
         $bookings = $bookingRepository->findByDateRange($dateStart, $dateEnd);
 
         $profits = $bookingService->calculateProfits($bookings);
+
+        // count all days from 1 january to today
+        $dayCount = (int) (new DateTime())->diff($dateStart)->format('%a');
+
+//        $profitsPerMonth = [];
+//        for ($i = 1; $i <= $monthCount; $i++) {
+//            $dateStart = new DateTime("first day of January + $i month");
+//            $dateEnd = new DateTime("last day of January + $i month");
+//            $profitsPerMonth[] = [
+//                'month' => $dateStart->format('F'),
+//                'profits' => $bookingService->calculateProfits($bookingRepository->findByDateRange($dateStart, $dateEnd)),
+//            ];
+//        }
+
+
         $profitsDeclared = $bookingService->calculateProfitsDeclared($bookings);
         $profitsUndeclared = $bookingService->calculateProfitsUndeclared($bookings);
 
@@ -27,12 +42,18 @@ class DashboardController extends AbstractController
         $profitsUndeclaredInt = (int) $bookingService->calculateProfitsUndeclared($bookings, false);
         $totalProfitsInt = $profitsUndeclaredInt + $profitsDeclaredInt;
 
+        $averageMonthProfits = $totalProfitsInt / $dayCount * 30;
+        // convert to euros
+        $averageMonthProfits = $bookingService->convertIntToEuros($averageMonthProfits);
+
+
         return $this->render('dashboard/index.html.twig', [
             'profitsDeclared' => $profitsDeclared,
             'profitsDeclaredInt' => $profitsDeclaredInt,
             'profitsUndeclared' => $profitsUndeclared,
             'profitsUndeclaredInt' => $profitsUndeclaredInt,
             'totalProfits' => $totalProfitsInt,
+            'averageMonthProfits' => $averageMonthProfits,
         ]);
     }
 }
